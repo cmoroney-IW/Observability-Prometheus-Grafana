9d0e0b243492e64ea62cc1c0f56a8f70e6867a80
cmoroney-IW <campbell.moroney@integration.works>
2023-01-17 10:06:56 +1300

    Working. Updated postman collection

--- Script/script.sh
+++ Script/script.sh
@@ -15,38 +15,38 @@ done
 curl -i -X POST \
 --url http://kong:8001/services/ \
 --data name=GetAllCustomers \
---data 'url=http://postgres-spring:8080/customer/all'
+--data 'url=http://app:8080/customer/all'
 curl -i -X POST \
 --url http://kong:8001/services/GetAllCustomers/routes/ \
     -H "Content-Type: application/json" \
-    -d '{"name": "allCustomers", "methods":["GET"], "hosts":["localhost"], "paths":["/customer"]}'
+    -d '{"name": "getAllCustomers", "methods":["GET"], "hosts":["localhost"], "paths":["/customer/all"]}'
 
 
 curl -i -X POST \
 --url http://kong:8001/services/ \
 --data name=CreateCustomer \
---data 'url=http://postgres-spring:8080/customer/create'
+--data 'url=http://app:8080/customer/create'
 curl -i -X POST \
 --url http://kong:8001/services/CreateCustomer/routes/ \
     -H "Content-Type: application/json" \
-    -d '{"name": "create", "methods":["POST"], "hosts":["localhost"], "paths":["/customer"],"headers":{"Content-Type":["application/json"]}}'
+    -d '{"name": "createCustomer", "methods":["POST"], "hosts":["localhost"], "paths":["/customer/create"],"headers":{"Content-Type":["application/json"]}}'
 
 
 curl -i -X POST \
 --url http://kong:8001/services/ \
 --data name=DeleteCustomer \
---data 'url=http://postgres-spring:8080/customer/delete'
+--data 'url=http://app:8080/customer/delete'
 curl -i -X POST \
 --url http://kong:8001/services/DeleteCustomer/routes/ \
     -H "Content-Type: application/json" \
-    -d '{"name": "delete", "methods":["DELETE"], "hosts":["localhost"], "paths":["/customer"]}'
+    -d '{"name": "deleteCustomer", "methods":["DELETE"], "hosts":["localhost"], "paths":["/customer/delete"]}'
 
 
 curl -i -X POST \
 --url http://kong:8001/services/ \
 --data name=GetCustomer \
---data 'url=http://postgres-spring:8080/customer'
+--data 'url=http://app:8080/customer'
 curl -i -X POST \
 --url http://kong:8001/services/GetCustomer/routes/ \
     -H "Content-Type: application/json" \
-    -d '{"name": "customer", "methods":["GET"], "hosts":["localhost"], "paths":["/customer"]}'
+    -d '{"name": "getCustomer", "methods":["GET"], "hosts":["localhost"], "paths":["/customer"]}'
